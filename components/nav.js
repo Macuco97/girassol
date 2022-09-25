6e0b9ca171594ed7d7472f35984dd9bfe09c8502
unknown <rapha-sm2015@hotmail.com>
2022-09-25 01:26:56 -0300

    Add Animation in NavContent

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
+import { motion, useAnimationControls } from 'framer-motion'
 
 export default function Nav({navContent, setNavContent}) {
-    const options = ['newborn', 'acompanhamento', 'familia', 'gestante']
-
-    const transition = position => {
+    useEffect(() => {
+        navContentAnimation()
+    })
+    const navContentAnimationControl = useAnimationControls()
+    const navContentAnimation = () => {
+        navContentAnimationControl.start({
+            opacity: [0, 1]
+        })
+    }
+    const navContentOptions = ['newborn', 'acompanhamento', 'familia', 'gestante']
+    const navContentTransition = position => {
         switch(position) {
             case 'right':
                 if(navContent === 3) {
@@ -22,17 +30,20 @@ export default function Nav({navContent, setNavContent}) {
                     setNavContent(navContent - 1)
                 }
         }
+        navContentAnimation()
     }
 
     return (
         <nav className = {`flex flex-row bg-amber-50 text-2xl uppercase text-center font-serif py-2 shadow-lg place-content-between items-center px-8`}>
-            <svg onClick = {() => transition('left')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-10 h-10`}>
+            <svg onClick = {() => navContentTransition('left')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-10 h-10`}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
             </svg>
-            <div className = {``}>
-                {options[navContent]}
-            </div>
-            <svg onClick = {() => transition('right')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-10 h-10`}>
+            <motion.div 
+            className = {``}
+            animate = {navContentAnimationControl}>
+                {navContentOptions[navContent]}
+            </motion.div>
+            <svg onClick = {() => navContentTransition('right')} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`w-10 h-10`}>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
             </svg>
         </nav>
